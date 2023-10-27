06aefee7a773e6660dbf938c8772bbd6e3acd9da
Valentine Briese <valentinegb@icloud.com>
2023-10-27 10:07:51 -0700

    Complete yr2015 dy1 pt1

--- src/years/yr2015/dy1.rs
+++ src/years/yr2015/dy1.rs
@@ -1,3 +1,13 @@
 pub(super) fn dy1(input: String) {
-    todo!();
+    let mut floor = 0;
+
+    for char in input.chars() {
+        match char {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => unreachable!("puzzle input should not contain characters other than '(' and ')'"),
+        }
+    }
+
+    println!("{floor}");
 }
