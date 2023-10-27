b38628e2207be1b75a096fa278def6ef44fc30f0
Valentine Briese <valentinegb@icloud.com>
2023-10-27 10:01:07 -0700

    Add description to `day` and `input` arguments

--- src/years.rs
+++ src/years.rs
@@ -8,8 +8,10 @@ use self::yr2015::Advent2015Day;
 pub(super) enum AdventYear {
     #[command(name = "2015")]
     Yr2015 {
+        /// Day of puzzle
         #[arg(value_enum)]
         day: Advent2015Day,
+        /// Puzzle input
         #[arg()]
         input: String,
     },
